7250eaf91199d456e92647453f1af11ff7002a31
Hemant Patil <112842857+hemant14050@users.noreply.github.com>
2024-05-19 17:48:53 +0530

    Revert "Created navbar and some routes"

--- airLicht-frontend/src/pages/AboutUs.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AboutUs = () => {
-  return <div className="text-richblack-5">AboutUs Page</div>;
-};
-
-export default AboutUs;
--- airLicht-frontend/src/pages/SignUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SignUp = () => {
-  return (
-    <div className="text-richblack-5">SignUp Page</div>
-  );
-};
-
-export default SignUp;
