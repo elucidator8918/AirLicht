a7dcd73a104a181322992b049762d1baa3c24f85
hemant14050 <hemant14050@gmail.com>
2024-05-19 16:37:55 +0530

    created navbar and some routes

---
+++ airLicht-frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-richblack-5">This is Home page</div>
+  );
+};
+
+export default Home;
---
+++ airLicht-frontend/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="text-richblack-5">SignUp Page</div>
+  );
+};
+
+export default SignUp;
